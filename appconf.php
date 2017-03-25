2bf40500c90a2812eb1cac2e1c58545751bdc201
小丁工作室 <ddawx123@users.noreply.github.com>
2017-03-25 19:26:20 +0800

    Update appconf.php

--- appconf.php
+++ appconf.php
@@ -6,21 +6,21 @@ Please use this sql command to install it.
 SQL Code:
 ############################################
 #create table if not exists spectaculars ( #
-#	id int auto_increment primary key,	   #
-#	info varchar(1000),					   #
-#	uip varchar(100),                      #
-#   utime varchar(100)                     #
-#	)default charset=utf8;				   #
+#	id int auto_increment primary key,	     #
+#	info varchar(1000),					             #
+#	uip varchar(100),                        #
+# utime varchar(100)                       #
+#	)default charset=utf8;				           #
 ############################################
 Powered by DingStudio.Tech(Club) Copyright 2017
 */
 
-define('mysql_server_name','sqld-hk.bcehost.com:4050');
-define('mysql_username','eadd490f0da843d2b9e8d3a5cdb49982');
-define('mysql_password','ae6afa67c9f049d490eb4e63483e9a57');
-define('mysql_dbname','hEypVFlLFQsBcOBjdVbf');
+define('mysql_server_name','sqld.bcehost.com:3306');
+define('mysql_username','u28');
+define('mysql_password','123456');
+define('mysql_dbname','msgwall_db');
 define('adminer','root');
-define('adminpasswd','99002103a');
+define('adminpasswd','123456');
 
 define('enabled','true');
 define('AddEnabled','true');
